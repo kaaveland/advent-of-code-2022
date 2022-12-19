8d3bc3739d3a66c1b004e4ec0e6250c46cf9538f
Robin Kåveland <kaaveland@gmail.com>
2022-12-19 23:22:50 +0100

    Pass day 17 example test case

--- src/bin/day_17/main.rs
+++ src/bin/day_17/main.rs
@@ -1,24 +1,7 @@
+use anyhow::Result;
+use aoc::io::read_stdin;
 use itertools::Itertools;
 use std::cmp::max;
-
-const SHAPES: &str = "####
-
-.#.
-###
-.#.
-
-..#
-..#
-###
-
-#
-#
-#
-#
-
-##
-##
-";
 
 type Shape = Vec<(i64, i64)>;
 
@@ -26,7 +9,7 @@ fn shapes() -> Vec<Shape> {
     vec![
         vec![(0, 0), (1, 0), (2, 0), (3, 0)],             // hline
         vec![(0, -1), (1, 0), (1, -1), (1, -2), (2, -1)], // cross
-        vec![(0, 0), (1, 0), (2, 0), (2, -1), (2, -2)],   // reverse L
+        vec![(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],     // reverse L
         vec![(0, 0), (0, -1), (0, -2), (0, -3)],          // vline
         vec![(0, 0), (0, -1), (1, 0), (1, -1)],           // square
     ]
@@ -59,7 +42,7 @@ const MAX_HEIGHT: usize = 10000;
 fn in_bounds(shape: &Shape) -> bool {
     shape
         .iter()
-        .all(|&(x, y)| x >= 0 && x < CHAMBER_WIDTH && y >= 0)
+        .all(|&(x, y)| (0..CHAMBER_WIDTH).contains(&x) && y >= 0)
 }
 
 fn drop_rock(
@@ -72,9 +55,8 @@ fn drop_rock(
     let height = max_heights.iter().max().unwrap();
     //  bottom left
     let mut shape = shape.clone();
-    let mut y_low = shape.iter().map(|&(_, y)| y).min().unwrap();
+    let y_low = shape.iter().map(|&(_, y)| y).min().unwrap();
     shift(&mut shape, (2, height + 3 + y_low.abs()));
-    println!("Min shape y = {:?}", height + 3 + y_low.abs());
 
     loop {
         // Invariant: we're in bounds here, enforce it
@@ -82,7 +64,6 @@ fn drop_rock(
             panic!("Out of bounds {time} {shape:?}");
         }
         let jet = &jets[time % jets.len()];
-        println! {"{jet:?}"};
         let (forward, backward) = match jet {
             Jet::Right => ((1, 0), (-1, 0)),
             Jet::Left => ((-1, 0), (1, 0)),
@@ -94,7 +75,7 @@ fn drop_rock(
 
         if shape
             .iter()
-            .any(|&(x, y)| x < 0 || x >= CHAMBER_WIDTH || grid[y as usize][x as usize])
+            .any(|&(x, y)| !(0..CHAMBER_WIDTH).contains(&x) || grid[y as usize][x as usize])
         {
             // Preserve invariant
             shift(&mut shape, backward);
@@ -123,32 +104,21 @@ fn drop_rock(
 
 fn drop_many_rocks(jets: &Jets, rocks_to_drop: usize) -> i64 {
     let shapes = shapes();
-    let mut grid = vec![vec![false; CHAMBER_WIDTH as usize]; MAX_HEIGHT as usize];
+    let mut grid = vec![vec![false; CHAMBER_WIDTH as usize]; MAX_HEIGHT];
     let mut max_heights = [0; CHAMBER_WIDTH as usize];
     let mut time = 0;
     for rock_number in 0..rocks_to_drop {
         let shape = &shapes[rock_number % shapes.len()];
         (time, max_heights) = drop_rock(jets, shape, time, &max_heights, &mut grid);
-        println!("Time: {time} {max_heights:?}");
+        println!("{max_heights:?} {time}");
     }
-    for row in (0..20).rev() {
-        let ch: String = (0..CHAMBER_WIDTH)
-            .map(|x| {
-                if grid[row as usize][x as usize] {
-                    '#'
-                } else {
-                    '.'
-                }
-            })
-            .collect();
-        println!("{ch}");
-    }
+
     *max_heights.iter().max().unwrap()
 }
 
 #[cfg(test)]
 pub mod tests {
-    use crate::{drop_many_rocks, drop_rock, shapes, Jet};
+    use crate::{drop_many_rocks, Jet};
     const EXAMPLE: &str = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
 
     #[test]
@@ -159,32 +129,16 @@ pub mod tests {
     }
 
     #[test]
-    fn print_shapes() {
-        let mut i = 0;
-        for mut shape in shapes() {
-            let xmin = shape.iter().map(|&(x, _)| x).min().unwrap();
-            let ymin = shape.iter().map(|&(_, y)| y).min().unwrap();
-            super::shift(&mut shape, (-xmin, -ymin));
-            let mut disp = [[false; 5]; 5];
-            for (x, y) in shape {
-                disp[y as usize][x as usize] = true;
-            }
-            println!("Shape: {i}");
-            i += 1;
-            for row in (0..5).rev() {
-                let ch: String = (0..5)
-                    .map(|r| if disp[row][r] { '#' } else { '.' })
-                    .collect();
-                println!("{ch}");
-            }
-        }
-    }
-
-    #[test]
     fn test_drop_many_rocks() {
         let jets = super::parse_jets(EXAMPLE);
-        let answer = drop_many_rocks(&jets, 3);
+        let answer = drop_many_rocks(&jets, 2022);
         assert_eq!(answer, 3068);
     }
 }
-fn main() {}
+fn main() -> Result<()> {
+    let inp = read_stdin()?;
+    let jets = parse_jets(inp.as_str());
+    let max_height = drop_many_rocks(&jets, 2022);
+    println!("{max_height}");
+    Ok(())
+}
