15898ca4b7c2182fe7bea9855aee875834fd01bc
Robin Kåveland <kaaveland@gmail.com>
2022-12-29 14:12:59 +0100

    Ask for a user agent string

--- src/bin/get_available_inputs.rs
+++ src/bin/get_available_inputs.rs
@@ -1,17 +1,30 @@
 use anyhow::Result;
 use aoc::io::read_stdin;
 use reqwest::blocking::ClientBuilder;
-use std::fs;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
+use std::{fs, io};
+
+fn obtain_user_agent() -> Result<String> {
+    println!("Enter a user agent string containing a way to contact you: ");
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf)?;
+    Ok(buf.trim().to_string())
+}
+
+fn obtain_session_cookie() -> Result<String> {
+    println!("Enter your session cookie");
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf)?;
+    Ok(buf.trim().replace("session=", "").replace('"', ""))
+}
 
 fn main() -> Result<()> {
-    let cookie_input = read_stdin()?;
+    let cookie_input = obtain_session_cookie()?;
+    let user_agent = obtain_user_agent()?;
     let cookie = format!("session={}", cookie_input.trim());
-    let client = ClientBuilder::new()
-        .user_agent("rust/reqwest/kaaveland@gmail.com")
-        .build()?;
+    let client = ClientBuilder::new().user_agent(user_agent).build()?;
 
     for day in 1..=25 {
         let folder = format!("./input/day_{day:0>2}");
